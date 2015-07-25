44d93062b1abcf965fcfb9babb4169aca5848c70
geigerenergy <info@geiger-energy.de>
2015-07-26 00:20:19 +0200

    Saved results

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix based on a standard matrix
+## that was fed to the function. It includes generating the utility functions
+## (getter and setter) that will be used to obtain the inverse if present.
+## cacheSolve gets the matrix and tries to read the inverse. If it was computed
+## it is obtained otherwise, it will compute the inverse and save it to the 
+## special matrix
 
-## Write a short comment describing this function
+## gets matrix, defines variables and getter and setter functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+  inverse_matrix <- NULL
+  set <- function(y) {
+    matrix <<- y
+    inverse_matrix <<- NULL
+  }
+  get <- function() matrix
+  setinverse <- function(inverse) inverse_matrix <<- inverse
+  getinverse <- function() inverse_matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## reads the inverse, if null is returned, copute inverse and save to matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+  inverse_matrix <- matrix$getinverse()
+  if(!is.null(inverse_matrix)) {
+    message("getting cached matrix data")
+    return(inverse_matrix)
+  }
+  data <- matrix$get()
+  inverse_matrix <- solve(data, ...)
+  matrix$setinverse(inverse_matrix)
+  inverse_matrix
 }
